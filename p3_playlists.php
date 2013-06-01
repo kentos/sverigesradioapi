c4b618b3f3b3a163221cc1f55c74f50c6d749eed
Kent Cederström <kent.cederstrom@strateg.se>
2013-06-01 15:06:57 +0200

    Cleaned primary file

--- p3_playlists.php
+++ p3_playlists.php
@@ -10,35 +10,5 @@ date_default_timezone_set('UTC');
 $base_url = "http://sverigesradio.se/sida/latlista.aspx?programid=164&date=";
 $date = date("Y-m-d");
 
-	
-/* main loop for playlists */
-for($i=0; $i <= 30; $i++) {
-	$date = date("Y-m-d", strtotime("-". $i ." day"));
-	$doc = $base_url . $date;
-	
-	$content = cache_get_contents($doc);
-	
-	phpQuery::newDocument($content)->find(".songlist");
-	
-	foreach(pq("tbody tr") as $tr) {
-		
-		if(pq($tr)->hasClass("expand-container")) {
-			$json[$current_index] = (object) array(); // How the fuck do you do a object the easiest way??
-			$json[$current_index]->time = $time = pq("td.time", $tr)->text();
-			$json[$current_index]->artist = trim(pq("span.artist", $tr)->text());
-			$json[$current_index]->song = trim(pq("span.title", $tr)->text());
-			$json[$current_index]->date = $date;
-		} else {
-			$json[$current_index]->composerinfo = str_replace("Kompositör: ", "", pq("span.composerinfo", $tr)->text());
-			$json[$current_index]->spotifylink = pq("a.spotify", $tr)->attr("href");
-			
-			$current_index++;
-		}
-		
-		/* dev purpuse: prints the first 40 */
-		if($current_index==40) {
-			echo json_encode($json);
-			exit;
-		}
-	}
-}
+$json = get_playlist_by_date($date);
+
