5466a74987ac34d2d6de0d5e298c1928b7c0764b
Kent Cederström <kent.cederstrom@strateg.se>
2013-06-02 01:21:35 +0200

    changes

--- api/index.php
+++ api/index.php
@@ -1,3 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sveriges Radio API</title>
+</head>
+<body>
 
-include("../app/config.inc.php");
+	<h1>Sveriges Radio API</h1>
+	
+	<p>
+		<table cellpadding="5">
+			<tr>
+				<th align="left" width="200">Method</th>
+				<th align="left">What to expect</th>
+			</tr>
+			
+			<tr>
+				<td valign="top">/api/v1/hello</td>
+				<td valign="top">Handshake. Returns current date.</td>
+			</tr>
+			
+			<tr>
+				<td valign="top">/api/v1/artist?s=%s</td>
+				<td valign="top">Search for artist and return a list of plays</td>
+			</tr>
+			
+			<tr>
+				<td valign="top">/api/v1/toplist?start=%d&end=%d</td>
+				<td valign="top">Get a toplist of most played songs</td>
+			</tr>
+		</table>
+	</p>
+
+</body>
+</html>
